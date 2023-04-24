5bab8260ca08a4ce1d71e3159576355006dcb8e0
Rudyboy <rudylepretre@gmail.com>
2023-04-24 18:56:54 +0200

    First scope of the drawer menu

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.openclassrooms.realestatemanager.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -16,15 +19,60 @@ class MainActivity : AppCompatActivity() {
     private val binding by viewBinding { MainActivityBinding.inflate(it) }
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        initUi()
+    }
 
+    private fun initUi() {
+        setContentView(binding.root)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        initDrawerLayout()
+        initNavigationView()
+        setupHeaderView()
+    }
+
+    private fun initDrawerLayout() {
+        drawerLayout = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, binding.toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun initNavigationView() {
+        val navView = binding.navigationView
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_item_map -> {
+                    // Naviguer vers la carte
+                    true
+                }
+                R.id.menu_item_add_property -> {
+                    // Naviguer vers l'interface d'ajout de bien immobilier
+                    true
+                }
+                R.id.menu_item_mortgage_calculator -> {
+                    // Naviguer vers le simulateur de prêt immobilier
+                    true
+                }
+                else -> false
+            }.also {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+        }
+    }
+
+    private fun setupHeaderView() {
+        // Configuration du header de la NavigationView
+//        val headerView = navView.getHeaderView(0)
     }
 }
