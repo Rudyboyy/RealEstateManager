6cc75690a831447c6af557df4d77278761ef15b9
Rudyboy <rudylepretre@gmail.com>
2023-05-04 11:12:34 +0200

    add function to handle back pressed in fragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/map/MapFragment.kt
@@ -9,19 +9,26 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.MapFragmentBinding
+import com.openclassrooms.realestatemanager.utils.FragmentUtils.handleBackPressed
 import com.openclassrooms.realestatemanager.utils.viewBinding
 
 class MapFragment : Fragment(R.layout.map_fragment), OnMapReadyCallback {
 
     private val binding by viewBinding { MapFragmentBinding.bind(it) }
     private lateinit var map: GoogleMap
+    private val actionFragment: Int = R.id.action_MapFragment_to_PropertyListFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+        handleBackPressed(R.id.action_MapFragment_to_PropertyListFragment)
+        setBackButton()
+    }
+
+    private fun setBackButton() {
         binding.backButton.setOnClickListener {
-            findNavController().navigate(R.id.action_MapFragment_to_PropertyListFragment)//todo a voir
+            findNavController().navigate(actionFragment)
         }
     }
 
