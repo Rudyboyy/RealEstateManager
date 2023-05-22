c42e4a84fad07aac2ac95814ed99904a9e19e8ed
Rudyboy <rudylepretre@gmail.com>
2023-05-22 12:29:00 +0200

    first scope of RealEstateContentProvider

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/provider/RealEstateContentProvider.kt
@@ -0,0 +1,76 @@
+package com.openclassrooms.realestatemanager.provider
+
+import android.content.ContentProvider
+import android.content.ContentUris
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import com.openclassrooms.realestatemanager.database.AppDatabase
+import com.openclassrooms.realestatemanager.database.dao.PropertyDao
+import com.openclassrooms.realestatemanager.model.Property
+import kotlinx.coroutines.*
+
+class RealEstateContentProvider : ContentProvider() {
+
+    private lateinit var propertyDao: PropertyDao
+
+    override fun onCreate(): Boolean {
+        context?.let {
+            val database = AppDatabase.getDatabase(it)
+            propertyDao = database.realEstateDao()
+            return true
+        }
+        return false
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        val propertyId = ContentUris.parseId(uri)
+        return propertyDao.getPropertiesWithCursor(propertyId)
+    }
+
+    override fun getType(uri: Uri): String? {
+        return "vnd.android.cursor.dir/${context?.packageName}.realEstate"
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        values?.let {
+            val property = Property.fromContentValues(values)
+            CoroutineScope(Dispatchers.IO).launch {
+                propertyDao.insert(property)
+            }
+            context?.contentResolver?.notifyChange(uri, null)
+            return ContentUris.withAppendedId(uri, property.id)
+        }
+        throw IllegalArgumentException("Failed to insert row for uri $uri")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        values?.let {
+            val property = Property.fromContentValues(values)
+            var count = 0
+            runBlocking {
+                withContext(Dispatchers.IO) {
+                    count = propertyDao.update(property)
+                }
+            }
+            context?.contentResolver?.notifyChange(uri, null)
+            return count
+        }
+        throw IllegalArgumentException("Failed to update row for uri $uri")
+    }
+}
