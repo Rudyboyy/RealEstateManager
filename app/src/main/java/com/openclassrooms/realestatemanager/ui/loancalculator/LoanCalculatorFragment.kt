a54e9b0f696e0dbcc4cc82b113f0204936e527e6
Rudyboy <rudylepretre@gmail.com>
2023-06-11 17:33:40 +0200

    Add function to convert currency in euro in loanCalculator

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/loancalculator/LoanCalculatorFragment.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/loancalculator/LoanCalculatorFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.SeekBar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.LoanCalculatorFragmentBinding
+import com.openclassrooms.realestatemanager.injection.Injection
+import com.openclassrooms.realestatemanager.utils.Utils
 import com.openclassrooms.realestatemanager.utils.viewBinding
+import com.openclassrooms.realestatemanager.viewmodels.RealEstateViewModel
 import java.text.DecimalFormat
 import kotlin.math.pow
 
 class LoanCalculatorFragment : Fragment(R.layout.loan_calculator_fragment) {
 
     private val binding by viewBinding { LoanCalculatorFragmentBinding.bind(it) }
+    private val viewModel: RealEstateViewModel by activityViewModels {
+        Injection.provideViewModelFactory(requireContext())
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,44 +28,25 @@ class LoanCalculatorFragment : Fragment(R.layout.loan_calculator_fragment) {
         binding.seekBarLoanTerm.max = 25 - 2
         binding.seekBarInterestRate.max = 500
 
-        binding.seekBarLoanAmount.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
+        setupSeekBarListeners()
+        updateCurrency()
+    }
+
+    private fun setupSeekBarListeners() {
+        val seekBarListeners = object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 updateMonthlyPayment()
+                updateCurrency()
             }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-        })
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        }
 
-        binding.seekBarLoanTerm.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                updateMonthlyPayment()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-        })
-
-        binding.seekBarInterestRate.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                updateMonthlyPayment()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-        })
+        binding.seekBarLoanAmount.setOnSeekBarChangeListener(seekBarListeners)
+        binding.seekBarLoanTerm.setOnSeekBarChangeListener(seekBarListeners)
+        binding.seekBarInterestRate.setOnSeekBarChangeListener(seekBarListeners)
     }
 
     private fun formatAmount(amount: Double): String {
@@ -97,7 +85,6 @@ class LoanCalculatorFragment : Fragment(R.layout.loan_calculator_fragment) {
             formatAmount(totalInterest)
         }
 
-
         binding.monthlyPaymentValue.text =
             getString(R.string.payment_per_month, monthlyPaymentFormatted)
         binding.loanAmountValue.text = getString(R.string.price_format_dollar, loanAmountFormatted)
@@ -105,6 +92,77 @@ class LoanCalculatorFragment : Fragment(R.layout.loan_calculator_fragment) {
         binding.interestRateValue.text = getString(R.string.interest_rate_value, interestRate)
         binding.seekBarInterestRate.progress = (interestRate * 100).toInt()
         binding.totalInterestValue.text = getString(R.string.total_interest, totalInterestFormatted)
+    }
+
+    private fun updateCurrency() {
+        viewModel.currencyLiveData.observe(viewLifecycleOwner) { isDollar ->
+            val loanAmount = (binding.seekBarLoanAmount.progress * 5000).toDouble() + 100000
+            val loanTerm = binding.seekBarLoanTerm.progress + 2
+            val currentInterestRate = binding.seekBarInterestRate.progress.toDouble() / 100
+            val interestRate = if (binding.seekBarLoanTerm.isPressed) {
+                when (loanTerm) {
+                    in 2..9 -> 3.12
+                    in 10..12 -> 3.21
+                    in 13..19 -> 3.27
+                    in 20..24 -> 3.41
+                    25 -> 3.58
+                    else -> currentInterestRate
+                }
+            } else {
+                currentInterestRate
+            }
+            val monthlyPayment = calculateLoanMonthlyPayment(loanAmount, interestRate, loanTerm)
+            val loanDurationInMonths = loanTerm * 12
+            val totalInterest = (monthlyPayment * loanDurationInMonths) - loanAmount
+
+            val loanAmountFormatted = if (isDollar) {
+                loanAmount
+            } else {
+                Utils.convertDollarToEuro(loanAmount.toInt()).toDouble()
+            }
+
+            val priceFormatResId = if (isDollar) {
+                R.string.price_format_dollar
+            } else {
+                R.string.price_format_eur
+            }
+
+            var monthlyPaymentFormatted = if (isDollar) {
+                monthlyPayment
+            } else {
+                Utils.convertDollarToEuro(monthlyPayment.toInt()).toDouble()
+            }
+
+            val priceFormatResId2 = if (isDollar) {
+                R.string.payment_per_month
+            } else {
+                R.string.payment_per_month_euro
+            }
+
+            var totalInterestFormatted = if (isDollar) {
+                totalInterest
+            } else {
+                Utils.convertDollarToEuro(totalInterest.toInt()).toDouble()
+            }
+
+            val priceFormatResId3 = if (isDollar) {
+                R.string.total_interest
+            } else {
+                R.string.total_interest_euro
+            }
+
+            if (monthlyPayment.isNaN()) {
+                totalInterestFormatted = 0.0
+                monthlyPaymentFormatted = loanAmountFormatted / (loanTerm * 12)
+            }
+
+            binding.loanAmountValue.text =
+                getString(priceFormatResId, formatAmount(loanAmountFormatted))
+            binding.monthlyPaymentValue.text =
+                getString(priceFormatResId2, formatAmount(monthlyPaymentFormatted))
+            binding.totalInterestValue.text =
+                getString(priceFormatResId3, formatAmount(totalInterestFormatted))
+        }
     }
 
     private fun calculateLoanMonthlyPayment(
